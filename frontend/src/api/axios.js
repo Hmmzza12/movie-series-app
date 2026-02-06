4a6d5f5fd63f7cb7b3c68011ba64cafd37f65f77
Hamza <hamza@local.dev>
2026-02-06 17:43:36 +0100

    chore: add render.yaml blueprint and robust api url handling

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_URL } from '../utils/constants';
 
 const axiosInstance = axios.create({
-    baseURL: API_URL
+    baseURL: API_URL.endsWith('/api') ? API_URL : `${API_URL}/api`
 });
 
 // Request interceptor to add auth token
